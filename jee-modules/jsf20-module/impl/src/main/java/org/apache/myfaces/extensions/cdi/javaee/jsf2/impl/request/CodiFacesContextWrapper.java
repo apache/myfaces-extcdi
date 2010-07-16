b2eb78662228995c7c969b33df91fef617f6ebbf
Gerhard Petracek <gpetracek@apache.org>
2010-07-16 13:20:17 +0000

    EXTCDI-1, EXTCDI-2 and EXTCDI-3 changes for jsf 2.0
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@964795 13f79535-47bb-0310-9956-ffa450edef68

--- jee-modules/jsf20-module/impl/src/main/java/org/apache/myfaces/extensions/cdi/javaee/jsf2/impl/request/CodiFacesContextWrapper.java
+++ jee-modules/jsf20-module/impl/src/main/java/org/apache/myfaces/extensions/cdi/javaee/jsf2/impl/request/CodiFacesContextWrapper.java
@@ -20,6 +20,7 @@ package org.apache.myfaces.extensions.cdi.javaee.jsf2.impl.request;
 
 import org.apache.myfaces.extensions.cdi.core.api.manager.BeanManagerProvider;
 import org.apache.myfaces.extensions.cdi.javaee.jsf.impl.request.BeforeAfterFacesRequestBroadcaster;
+import org.apache.myfaces.extensions.cdi.javaee.jsf.impl.scope.conversation.RedirectedConversationAwareExternalContext;
 
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseStream;
@@ -112,7 +113,7 @@ class CodiFacesContextWrapper extends FacesContext
 
     public ExternalContext getExternalContext()
     {
-        return wrappedFacesContext.getExternalContext();
+        return new RedirectedConversationAwareExternalContext(wrappedFacesContext.getExternalContext());
     }
 
     public FacesMessage.Severity getMaximumSeverity()
