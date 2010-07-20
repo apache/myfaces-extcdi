5fd3ae1d3e0a2c7f5728d5950e5a6338c0fd6e9f
Gerhard Petracek <gpetracek@apache.org>
2010-07-20 09:24:33 +0000

    EXTCDI-1, EXTCDI-2 and EXTCDI-3 cleanup
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@965770 13f79535-47bb-0310-9956-ffa450edef68

--- jee-modules/jsf20-module/impl/src/main/java/org/apache/myfaces/extensions/cdi/javaee/jsf2/impl/request/CodiFacesContextWrapper.java
+++ jee-modules/jsf20-module/impl/src/main/java/org/apache/myfaces/extensions/cdi/javaee/jsf2/impl/request/CodiFacesContextWrapper.java
@@ -22,31 +22,18 @@ import org.apache.myfaces.extensions.cdi.core.api.manager.BeanManagerProvider;
 import org.apache.myfaces.extensions.cdi.javaee.jsf.impl.request.BeforeAfterFacesRequestBroadcaster;
 import org.apache.myfaces.extensions.cdi.javaee.jsf2.impl.scope.conversation.RedirectedConversationAwareExternalContext;
 
-import javax.faces.context.FacesContext;
-import javax.faces.context.ResponseStream;
-import javax.faces.context.PartialViewContext;
-import javax.faces.context.ResponseWriter;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.ExceptionHandler;
-import javax.faces.application.Application;
-import javax.faces.application.FacesMessage;
-import javax.faces.application.ProjectStage;
-import javax.faces.render.RenderKit;
-import javax.faces.component.UIViewRoot;
-import javax.faces.event.PhaseId;
-import javax.el.ELContext;
+import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
-import javax.enterprise.context.spi.CreationalContext;
-import java.util.Iterator;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.context.FacesContextWrapper;
 import java.util.Set;
-import java.util.Map;
-import java.util.List;
 
 /**
  * @author Gerhard Petracek
  */
-class CodiFacesContextWrapper extends FacesContext
+class CodiFacesContextWrapper extends FacesContextWrapper
 {
     private FacesContext wrappedFacesContext;
 
@@ -81,34 +68,9 @@ class CodiFacesContextWrapper extends FacesContext
         this.beforeAfterFacesRequestBroadcaster.broadcastAfterFacesRequestEvent(this);
     }
 
-    public ELContext getELContext()
+    public FacesContext getWrapped()
     {
-        return wrappedFacesContext.getELContext();
-    }
-
-    public ExceptionHandler getExceptionHandler()
-    {
-        return wrappedFacesContext.getExceptionHandler();
-    }
-
-    public Application getApplication()
-    {
-        return wrappedFacesContext.getApplication();
-    }
-
-    public Map<Object, Object> getAttributes()
-    {
-        return wrappedFacesContext.getAttributes();
-    }
-
-    public Iterator<String> getClientIdsWithMessages()
-    {
-        return wrappedFacesContext.getClientIdsWithMessages();
-    }
-
-    public PhaseId getCurrentPhaseId()
-    {
-        return wrappedFacesContext.getCurrentPhaseId();
+        return this.wrappedFacesContext;
     }
 
     public ExternalContext getExternalContext()
@@ -116,141 +78,10 @@ class CodiFacesContextWrapper extends FacesContext
         return new RedirectedConversationAwareExternalContext(wrappedFacesContext.getExternalContext());
     }
 
-    public FacesMessage.Severity getMaximumSeverity()
-    {
-        return wrappedFacesContext.getMaximumSeverity();
-    }
-
-    public List<FacesMessage> getMessageList()
-    {
-        return wrappedFacesContext.getMessageList();
-    }
-
-    public List<FacesMessage> getMessageList(String s)
-    {
-        return wrappedFacesContext.getMessageList(s);
-    }
-
-    public Iterator<FacesMessage> getMessages()
-    {
-        return wrappedFacesContext.getMessages();
-    }
-
-    public Iterator<FacesMessage> getMessages(String s)
-    {
-        return wrappedFacesContext.getMessages(s);
-    }
-
-    public PartialViewContext getPartialViewContext()
-    {
-        return wrappedFacesContext.getPartialViewContext();
-    }
-
-    public RenderKit getRenderKit()
-    {
-        return wrappedFacesContext.getRenderKit();
-    }
-
-    public boolean getRenderResponse()
-    {
-        return wrappedFacesContext.getRenderResponse();
-    }
-
-    public boolean getResponseComplete()
-    {
-        return wrappedFacesContext.getResponseComplete();
-    }
-
-    public ResponseStream getResponseStream()
-    {
-        return wrappedFacesContext.getResponseStream();
-    }
-
-    public void setResponseStream(ResponseStream responseStream)
-    {
-        wrappedFacesContext.setResponseStream(responseStream);
-    }
-
-    public ResponseWriter getResponseWriter()
-    {
-        return wrappedFacesContext.getResponseWriter();
-    }
-
-    public boolean isValidationFailed()
-    {
-        return wrappedFacesContext.isValidationFailed();
-    }
-
-    public void setResponseWriter(ResponseWriter responseWriter)
-    {
-        wrappedFacesContext.setResponseWriter(responseWriter);
-    }
-
-    public UIViewRoot getViewRoot()
-    {
-        return wrappedFacesContext.getViewRoot();
-    }
-
-    public boolean isPostback()
-    {
-        return wrappedFacesContext.isPostback();
-    }
-
-    public boolean isProcessingEvents()
-    {
-        return wrappedFacesContext.isProcessingEvents();
-    }
-
-    public void setViewRoot(UIViewRoot uiViewRoot)
-    {
-        wrappedFacesContext.setViewRoot(uiViewRoot);
-    }
-
-    public void validationFailed()
-    {
-        wrappedFacesContext.validationFailed();
-    }
-
-    public boolean isProjectStage(ProjectStage projectStage)
-    {
-        return wrappedFacesContext.isProjectStage(projectStage);
-    }
-
-    public void addMessage(String s, FacesMessage facesMessage)
-    {
-        //TODO
-        wrappedFacesContext.addMessage(s, facesMessage);
-    }
-
     public void release()
     {
         broadcastAfterFacesRequestEvent();
         wrappedFacesContext.release();
-    }
-
-    public void renderResponse()
-    {
-        wrappedFacesContext.renderResponse();
-    }
-
-    public void responseComplete()
-    {
-        wrappedFacesContext.responseComplete();
-    }
-
-    public void setCurrentPhaseId(PhaseId phaseId)
-    {
-        wrappedFacesContext.setCurrentPhaseId(phaseId);
-    }
-
-    public void setExceptionHandler(ExceptionHandler exceptionHandler)
-    {
-        wrappedFacesContext.setExceptionHandler(exceptionHandler);
-    }
-
-    public void setProcessingEvents(boolean b)
-    {
-        wrappedFacesContext.setProcessingEvents(b);
     }
 
     private void initBroadcaster()
