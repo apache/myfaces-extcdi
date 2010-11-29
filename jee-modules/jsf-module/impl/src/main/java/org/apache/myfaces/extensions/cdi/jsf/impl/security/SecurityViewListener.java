278fbe03592b0c09ecf6ed2ab143977b21818220
Gerhard Petracek <gpetracek@apache.org>
2010-11-29 12:36:01 +0000

    EXTCDI-60 fallback if there isn't a view-root
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@1040088 13f79535-47bb-0310-9956-ffa450edef68

--- jee-modules/jsf-module/impl/src/main/java/org/apache/myfaces/extensions/cdi/jsf/impl/security/SecurityViewListener.java
+++ jee-modules/jsf-module/impl/src/main/java/org/apache/myfaces/extensions/cdi/jsf/impl/security/SecurityViewListener.java
@@ -19,7 +19,7 @@
 package org.apache.myfaces.extensions.cdi.jsf.impl.security;
 
 import org.apache.myfaces.extensions.cdi.jsf.api.listener.phase.AfterPhase;
-import org.apache.myfaces.extensions.cdi.jsf.api.listener.phase.JsfPhaseId;
+import static org.apache.myfaces.extensions.cdi.jsf.api.listener.phase.JsfPhaseId.*;
 import static org.apache.myfaces.extensions.cdi.jsf.impl.util.SecurityUtils.tryToHandleSecurityViolation;
 import org.apache.myfaces.extensions.cdi.jsf.impl.config.view.ViewConfigCache;
 import org.apache.myfaces.extensions.cdi.jsf.impl.config.view.ViewConfigEntry;
@@ -30,14 +30,33 @@ import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.faces.event.PhaseEvent;
 import javax.faces.context.FacesContext;
+import java.util.Map;
 
 /**
+ * If you don't need it or there is any issue, deactivate it via a CDI extension via
+ * {@link javax.enterprise.inject.spi.ProcessAnnotatedType#veto()} .
+ * 
  * @author Gerhard Petracek
  */
 public class SecurityViewListener
 {
-    public void checkPermission(
-            @Observes @AfterPhase(JsfPhaseId.RESTORE_VIEW) PhaseEvent event, BeanManager beanManager)
+    private static final String LAZY_SECURITY_CHECK_KEY = "LAZY_SECURITY_CHECK";
+
+    public void checkPermission(@Observes @AfterPhase(RESTORE_VIEW) PhaseEvent event, BeanManager beanManager)
+    {
+        FacesContext facesContext = event.getFacesContext();
+
+        if(facesContext.getViewRoot() == null)
+        {
+            triggerFallback(facesContext);
+            return;
+        }
+
+        checkPermission(beanManager, facesContext);
+    }
+
+    public void checkPermissionBeforeRendering(
+            @Observes @AfterPhase(RENDER_RESPONSE) PhaseEvent event, BeanManager beanManager)
     {
         FacesContext facesContext = event.getFacesContext();
 
@@ -47,6 +66,16 @@ public class SecurityViewListener
             return;
         }
 
+        Map<String, Object> requestMap = getRequestMap(facesContext);
+
+        if(requestMap != null && Boolean.TRUE.equals(requestMap.get(LAZY_SECURITY_CHECK_KEY)))
+        {
+            checkPermission(beanManager, facesContext);
+        }
+    }
+
+    private void checkPermission(BeanManager beanManager, FacesContext facesContext)
+    {
         ViewConfigEntry entry = ViewConfigCache.getViewDefinition(facesContext.getViewRoot().getViewId());
 
         if(entry == null)
@@ -63,5 +92,26 @@ public class SecurityViewListener
             tryToHandleSecurityViolation(accessDeniedException);
             facesContext.renderResponse();
         }
+    }
+
+    private void triggerFallback(FacesContext facesContext)
+    {
+        Map<String, Object> requestMap = getRequestMap(facesContext);
+
+        if(requestMap != null)
+        {
+            requestMap.put(LAZY_SECURITY_CHECK_KEY, Boolean.TRUE);
+        }
+    }
+
+    private Map<String, Object> getRequestMap(FacesContext facesContext)
+    {
+        if(facesContext.getExternalContext() == null || facesContext.getExternalContext().getRequestMap() == null)
+        {
+            //TODO log warning
+            return null;
+        }
+
+        return facesContext.getExternalContext().getRequestMap();
     }
 }
