28aeee0973b37ebd2c0a594bd6ebeb6697f9f8e5
Gerhard Petracek <gpetracek@apache.org>
2010-12-25 23:51:29 +0000

    EXTCDI-105 first draft
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@1052819 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test-modules/base-cargo-test-infrastructure-module/src/main/java/org/apache/myfaces/extensions/cdi/test/base/cargo/AbstractCargoTest.java
@@ -0,0 +1,113 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.cdi.test.base.cargo;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import org.apache.myfaces.extensions.cdi.jsf.impl.config.view.ViewConfigCache;
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * @author Gerhard Petracek
+ * @author Jakob Korherr
+ */
+public abstract class AbstractCargoTest
+{
+    private static final String CARGO_CONTEXT_PROPERTY = "cargo.context";
+    private static final String CARGO_PORT_PROPERTY = "cargo.port";
+
+    private static final String DEFAULT_CONTEXT = "cargo-test";
+    private static final String DEFAULT_PORT = "8080";
+
+    protected static String baseUrl = null;
+
+    protected WebClient webClient;
+
+    protected TestConfiguration getTestConfiguration()
+    {
+        return new TestConfiguration()
+        {
+            public WebClient getWebClient()
+            {
+                return webClient;
+            }
+
+            public String getBaseURL()
+            {
+                initBaseURL();
+                return baseUrl;
+            }
+
+            public boolean isCheckWindowId()
+            {
+                return true;
+            }
+        };
+    }
+
+    protected void initBaseURL()
+    {
+        if(baseUrl != null)
+        {
+            return;
+        }
+
+        String port = System.getProperty(CARGO_PORT_PROPERTY);
+        if (port == null)
+        {
+            port = DEFAULT_PORT;
+        }
+
+        String context = System.getProperty(CARGO_CONTEXT_PROPERTY);
+        if (context == null)
+        {
+            context = DEFAULT_CONTEXT;
+        }
+
+        baseUrl = "http://localhost:" + port + "/" + context + "/";
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        webClient = new WebClient(getBrowserVersion());
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        webClient.closeAllWindows();
+        webClient = null;
+        ViewConfigCache.reset();
+    }
+
+    /**
+     * Returns the Browser to use.
+     * Default is Firefox 3.6, override to change this.
+     *
+     * @return default browser
+     */
+    public BrowserVersion getBrowserVersion()
+    {
+        return BrowserVersion.FIREFOX_3_6;
+    }
+}
---
+++ test-modules/base-cargo-test-infrastructure-module/src/main/java/org/apache/myfaces/extensions/cdi/test/base/cargo/SimplePageInteraction.java
@@ -0,0 +1,233 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.cdi.test.base.cargo;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.http.conn.HttpHostConnectException;
+import org.apache.myfaces.extensions.cdi.core.api.config.view.ViewConfig;
+import org.apache.myfaces.extensions.cdi.jsf.impl.config.view.ViewConfigCache;
+import org.apache.myfaces.extensions.cdi.jsf.impl.config.view.ViewConfigExtension;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Gerhard Petracek
+ */
+public class SimplePageInteraction
+{
+    private WebClient webClient;
+
+    private String baseURL;
+
+    private HtmlPage currentPage;
+    private HtmlForm currentForm;
+
+    private String initialWindowId;
+    private String okButtonId = "ok";
+    private boolean checkWindowId;
+
+    public SimplePageInteraction(TestConfiguration testConfiguration)
+    {
+        this.webClient = testConfiguration.getWebClient();
+        this.baseURL = testConfiguration.getBaseURL();
+        this.checkWindowId = testConfiguration.isCheckWindowId();
+    }
+
+    public SimplePageInteraction with(Class<? extends ViewConfig> pageDefinition)
+    {
+        new ViewConfigExtension()
+        {
+            @Override
+            public void addPageDefinition(Class pageDefinitionClass)
+            {
+                super.addPageDefinition(pageDefinitionClass);
+            }
+        }.addPageDefinition(pageDefinition);
+        return this;
+    }
+
+    public SimplePageInteraction start(Class<? extends ViewConfig> pageDefinition)
+    {
+        this.currentPage = getPage(pageDefinition);
+        this.initialWindowId = getCurrentWindowId();
+
+        return this;
+    }
+
+    public SimplePageInteraction useDefaultForm()
+    {
+        return useForm("mainForm");
+    }
+
+    public SimplePageInteraction useForm(String formId)
+    {
+        this.currentForm = this.currentPage.getFormByName(formId);
+        return this;
+    }
+
+    public SimplePageInteraction useIdForOkButton(String okButtonId)
+    {
+        this.okButtonId = okButtonId;
+        return this;
+    }
+
+    public SimplePageInteraction setValue(String inputId, String value)
+    {
+        setInputValue(this.currentPage, inputId, value);
+        return this;
+    }
+
+    public SimplePageInteraction clickOk()
+    {
+        return clickOk(null);
+    }
+
+    public SimplePageInteraction clickOk(Class<? extends ViewConfig> expectedPage)
+    {
+        click(this.okButtonId, viewId(expectedPage));
+
+        if(expectedPage != null)
+        {
+            checkCurrentPage(expectedPage);
+        }
+
+        return this;
+    }
+
+    public SimplePageInteraction click(String id)
+    {
+        click(id, null);
+        return this;
+    }
+
+    public SimplePageInteraction checkState(Class<? extends ViewConfig> expectedPage)
+    {
+        checkCurrentPage(expectedPage);
+        checkCurrentWindowId();
+        return this;
+    }
+
+    public SimplePageInteraction checkCurrentPage(Class<? extends ViewConfig> expectedPage)
+    {
+        assertTrue(url(this.currentPage).contains(viewId(expectedPage)));
+        return this;
+    }
+
+    public SimplePageInteraction checkCurrentWindowId()
+    {
+        if(this.checkWindowId)
+        {
+            assertEquals(this.initialWindowId, getCurrentWindowId());
+        }
+        return this;
+    }
+
+    public SimplePageInteraction checkValue(String inputId, String expectedValue)
+    {
+        assertEquals(expectedValue, getValue(this.currentPage, inputId));
+        return this;
+    }
+
+    protected String getValue(HtmlPage htmlPage, String inputId)
+    {
+        return htmlPage.getElementById(inputId).getAttribute("value");
+    }
+
+    protected void setInputValue(HtmlPage htmlPage, String inputId, String value)
+    {
+        setInputValue(htmlPage, this.currentForm, inputId, value);
+    }
+
+    protected void setInputValue(HtmlPage htmlPage, HtmlForm htmlForm, String inputId, String value)
+    {
+        htmlForm.getInputByName(inputId).setValueAttribute(value);
+    }
+
+    protected String getCurrentWindowId()
+    {
+        //TODO
+        return this.currentPage.getUrl().getQuery();
+    }
+
+    protected HtmlPage getPage(Class<? extends ViewConfig> pageDefinition)
+    {
+        return getPage(ViewConfigCache.getViewDefinition(pageDefinition).getViewId());
+    }
+
+    protected HtmlPage getPage(String viewId)
+    {
+        try
+        {
+            if(viewId.startsWith("/"))
+            {
+                viewId = viewId.substring(1);
+            }
+
+            return this.webClient.getPage(this.baseURL + viewId);
+        }
+        catch (IOException e)
+        {
+            if(e instanceof HttpHostConnectException)
+            {
+                throw new ContainerNotStartedException();
+            }
+            else
+            {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    protected void click(String id, String expectedTarget)
+    {
+        try
+        {
+            this.currentPage = this.currentForm.getInputByName(id).click();
+            //TODO
+            useDefaultForm();
+
+            if(expectedTarget != null)
+            {
+                assertTrue(url(this.currentPage).contains(expectedTarget));
+            }
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected String viewId(Class<? extends ViewConfig> pageDefinition)
+    {
+        if(pageDefinition == null)
+        {
+            return null;
+        }
+        return ViewConfigCache.getViewDefinition(pageDefinition).getViewId();
+    }
+
+    protected String url(HtmlPage htmlPage)
+    {
+        return htmlPage.getUrl().toString();
+    }
+}
