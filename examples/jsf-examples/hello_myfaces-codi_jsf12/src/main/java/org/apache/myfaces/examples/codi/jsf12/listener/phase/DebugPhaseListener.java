0a87aad6ac7b28db3956a2884a9326aa5b79cc54
Gerhard Petracek <gpetracek@apache.org>
2011-01-12 10:11:13 +0000

    cleanup
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@1058043 13f79535-47bb-0310-9956-ffa450edef68

--- examples/jsf-examples/hello_myfaces-codi_jsf12/src/main/java/org/apache/myfaces/examples/codi/jsf12/listener/phase/DebugPhaseListener.java
+++ examples/jsf-examples/hello_myfaces-codi_jsf12/src/main/java/org/apache/myfaces/examples/codi/jsf12/listener/phase/DebugPhaseListener.java
@@ -18,35 +18,37 @@
  */
 package org.apache.myfaces.examples.codi.jsf12.listener.phase;
 
+import org.apache.myfaces.extensions.cdi.core.api.Advanced;
+import org.apache.myfaces.extensions.cdi.core.api.logging.Logger;
+
+import static org.apache.myfaces.extensions.cdi.core.api.projectstage.ProjectStage.*;
+
+import org.apache.myfaces.extensions.cdi.core.api.projectstage.ProjectStageActivated;
 import org.apache.myfaces.extensions.cdi.jsf.api.listener.phase.JsfPhaseListener;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 import javax.faces.event.PhaseListener;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
+import javax.inject.Inject;
 
+@Advanced
 @JsfPhaseListener
+@ProjectStageActivated(Development.class)
 public class DebugPhaseListener implements PhaseListener
 {
-    private static final Log LOG = LogFactory.getLog(DebugPhaseListener.class);
+    @Inject
+    private Logger logger;
 
     private static final long serialVersionUID = -3128296286005877801L;
 
     public void beforePhase(PhaseEvent phaseEvent)
     {
-        if(LOG.isDebugEnabled())
-        {
-            LOG.debug("before: " + phaseEvent.getPhaseId());
-        }
+        this.logger.info("before: " + phaseEvent.getPhaseId());
     }
 
     public void afterPhase(PhaseEvent phaseEvent)
     {
-        if(LOG.isDebugEnabled())
-        {
-            LOG.debug("after: " + phaseEvent.getPhaseId());
-        }
+        this.logger.info("after: " + phaseEvent.getPhaseId());
     }
 
     public PhaseId getPhaseId()
