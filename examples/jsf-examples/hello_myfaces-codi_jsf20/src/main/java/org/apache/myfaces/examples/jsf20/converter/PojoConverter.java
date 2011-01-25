72277089aca95c7fe3c21a6545981be72c0482bd
Jakob Korherr <jakobk@apache.org>
2011-01-25 19:05:47 +0000

    EXTCDI-127 Injection in FacesConverter does not work (sample)
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@1063405 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/jsf-examples/hello_myfaces-codi_jsf20/src/main/java/org/apache/myfaces/examples/jsf20/converter/PojoConverter.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.examples.jsf20.converter;
+
+import org.apache.myfaces.extensions.cdi.core.api.Advanced;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
+
+/**
+ * @author Jakob Korherr
+ */
+@Advanced
+@FacesConverter("pojoConverter")
+public class PojoConverter implements Converter
+{
+
+    public PojoConverter()
+    {
+        // set breakpoint for instance creation debugging here
+    }
+
+    @Inject
+    private PojoFactory pojoFactory;
+
+    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) throws ConverterException
+    {
+        return pojoFactory.getPojo(s);
+    }
+
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object o) throws ConverterException
+    {
+        if (o instanceof Pojo)
+        {
+            Pojo pojo = (Pojo) o;
+
+            return Integer.toString(pojo.getId());
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+}
---
+++ examples/jsf-examples/hello_myfaces-codi_jsf20/src/main/java/org/apache/myfaces/examples/jsf20/converter/PojoFactory.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.examples.jsf20.converter;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jakob Korherr
+ */
+@Singleton
+@Named
+public class PojoFactory
+{
+
+    private List<Pojo> pojos;
+
+    public List<Pojo> getPojos()
+    {
+        if (pojos == null)
+        {
+            // some demo values
+            pojos = new ArrayList<Pojo>();
+            pojos.add(new Pojo(1, "Frank"));
+            pojos.add(new Pojo(2, "Karl"));
+            pojos.add(new Pojo(3, "Adam"));
+        }
+
+        return pojos;
+    }
+
+    public Pojo getPojo(String idString)
+    {
+        if (idString == null)
+        {
+            return null;
+        }
+
+        return getPojo(parseId(idString));
+    }
+
+    public Pojo getPojo(int id)
+    {
+        for (Pojo pojo : getPojos())
+        {
+            if (pojo.getId() == id)
+            {
+                return pojo;
+            }
+        }
+
+        return null;
+    }
+
+    private int parseId(String idString) throws IllegalArgumentException
+    {
+        try
+        {
+            return Integer.parseInt(idString);
+        }
+        catch (NumberFormatException e)
+        {
+            throw new IllegalArgumentException("Illegal Pojo id: idString", e);
+        }
+    }
+
+}
