3f0386baec80b10ad8227358a2a75fafdc8c4f1c
Gerhard Petracek <gpetracek@apache.org>
2011-02-25 20:42:44 +0000

    reverted cleanup
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@1074694 13f79535-47bb-0310-9956-ffa450edef68

--- core/api/src/main/java/org/apache/myfaces/extensions/cdi/core/api/config/view/ManualView.java
+++ core/api/src/main/java/org/apache/myfaces/extensions/cdi/core/api/config/view/ManualView.java
@@ -25,5 +25,4 @@ package org.apache.myfaces.extensions.cdi.core.api.config.view;
  */
 public final class ManualView implements ViewConfig
 {
-    private static final long serialVersionUID = -2477132285461472286L;
 }
