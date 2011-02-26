2eec01c9453f61a99a79bb62351f5231a5a56759
Gerhard Petracek <gpetracek@apache.org>
2011-02-26 02:58:00 +0000

    simple integration test
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@1074783 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/jsf-examples/hello_myfaces-codi_jsf20/src/test/java/org/apache/myfaces/extensions/cdi/example/test/SimpleTestCase.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.cdi.example.test;
+
+import org.apache.myfaces.extensions.cdi.test.cargo.SimplePageInteraction;
+import org.apache.myfaces.extensions.cdi.test.cargo.runner.JUnit4WithCargo;
+import org.apache.myfaces.extensions.cdi.example.jsf20.view.config.Pages;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnit4WithCargo.class)
+public class SimpleTestCase extends BaseCargoTest
+{
+    // NOTE that new @Test means new WebClient means new WindowContext
+
+    @Test
+    public void testSimpleRegistration() throws Exception
+    {
+        SimplePageInteraction pageInteraction = new SimplePageInteraction(getTestConfiguration())
+                .with(Pages.Simple.Form.class)
+                .with(Pages.Simple.Summary.class);
+
+        pageInteraction
+                .start(Pages.Simple.Form.class)
+                .useForm("mainForm");
+
+        //prependId="false"
+        pageInteraction.setValue("loginName", "codi");
+        pageInteraction
+                .click("register")
+                .checkState(Pages.Simple.Summary.class);
+
+        pageInteraction.checkTextValue("loginName", "codi");
+
+        //close conversation
+        pageInteraction
+                .clickOk()
+                .checkState(Pages.Simple.Form.class);
+
+        pageInteraction.checkTextValue("loginName", "");
+    }
+}
