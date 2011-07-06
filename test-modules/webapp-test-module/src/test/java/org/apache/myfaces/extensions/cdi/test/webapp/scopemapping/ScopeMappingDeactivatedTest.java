e6b19d9fe4fc8b55211d0514e530495c0e793c0c
Gerhard Petracek <gpetracek@apache.org>
2011-07-06 09:47:22 +0000

    updated version numbers
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@1143320 13f79535-47bb-0310-9956-ffa450edef68

--- test-modules/webapp-test-module/src/test/java/org/apache/myfaces/extensions/cdi/test/webapp/scopemapping/ScopeMappingDeactivatedTest.java
+++ test-modules/webapp-test-module/src/test/java/org/apache/myfaces/extensions/cdi/test/webapp/scopemapping/ScopeMappingDeactivatedTest.java
@@ -63,12 +63,12 @@ import javax.faces.event.PhaseId;
 
 @WebappDependency.List
 ({
-    @WebappDependency("org.apache.myfaces.extensions.cdi.core:myfaces-extcdi-core-api:jar:1.0.0-SNAPSHOT"),
-    @WebappDependency("org.apache.myfaces.extensions.cdi.core:myfaces-extcdi-core-impl:jar:1.0.0-SNAPSHOT"),
-    @WebappDependency("org.apache.myfaces.extensions.cdi.modules:myfaces-extcdi-jsf20-module-api:jar:1.0.0-SNAPSHOT"),
-    @WebappDependency("org.apache.myfaces.extensions.cdi.modules:myfaces-extcdi-jsf20-module-impl:jar:1.0.0-SNAPSHOT"),
-    @WebappDependency("org.apache.myfaces.extensions.cdi.modules:myfaces-extcdi-message-module-api:jar:1.0.0-SNAPSHOT"),
-    @WebappDependency("org.apache.myfaces.extensions.cdi.modules:myfaces-extcdi-message-module-impl:jar:1.0.0-SNAPSHOT"),
+    @WebappDependency("org.apache.myfaces.extensions.cdi.core:myfaces-extcdi-core-api:jar:1.0.1-SNAPSHOT"),
+    @WebappDependency("org.apache.myfaces.extensions.cdi.core:myfaces-extcdi-core-impl:jar:1.0.1-SNAPSHOT"),
+    @WebappDependency("org.apache.myfaces.extensions.cdi.modules:myfaces-extcdi-jsf20-module-api:jar:1.0.1-SNAPSHOT"),
+    @WebappDependency("org.apache.myfaces.extensions.cdi.modules:myfaces-extcdi-jsf20-module-impl:jar:1.0.1-SNAPSHOT"),
+    @WebappDependency("org.apache.myfaces.extensions.cdi.modules:myfaces-extcdi-message-module-api:jar:1.0.1-SNAPSHOT"),
+    @WebappDependency("org.apache.myfaces.extensions.cdi.modules:myfaces-extcdi-message-module-impl:jar:1.0.1-SNAPSHOT"),
     @WebappDependency("org.apache.openwebbeans:openwebbeans-impl:jar:1.1.0"),
     @WebappDependency("org.apache.openwebbeans:openwebbeans-spi:jar:1.1.0"),
     @WebappDependency("org.apache.openwebbeans:openwebbeans-jsf:jar:1.1.0"),
