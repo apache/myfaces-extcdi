30299f00083b2752e2eb3e4e581ff75f36e9a92c
Mark Struberg <struberg@apache.org>
2011-11-04 08:05:25 +0000

    manually revert to -r1197092
    becaues 
    a.) history got trashed
    b.) we don't need all the abstract stuff
    
    
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@1197419 13f79535-47bb-0310-9956-ffa450edef68

--- jee-modules/jpa-module/api/src/main/java/org/apache/myfaces/extensions/cdi/jpa/api/datasource/ConfigurableDataSource.java
+++ jee-modules/jpa-module/api/src/main/java/org/apache/myfaces/extensions/cdi/jpa/api/datasource/ConfigurableDataSource.java
@@ -18,39 +18,204 @@
  */
 package org.apache.myfaces.extensions.cdi.jpa.api.datasource;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.myfaces.extensions.cdi.core.api.provider.BeanManagerProvider;
 
-import javax.enterprise.inject.Typed;
+import javax.naming.InitialContext;
 import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Map;
+import java.util.logging.Logger;
+import javax.naming.Context;
 
 /**
- * This class can be used for static config. and will resolve the final implementation at runtime.
+ * <p>This class can be used instead of a real DataSource.
+ * It is a simple wrapper to hide any database configuration details
+ * and make it configurable via CDI.</p>
  *
- * @author Gerhard Petracek
+ * <p>The configuration itself will be provided via CDI mechanics.
+ * To distinguish different databases, users can specify a
+ * <code>connectionId</code>. If no <code>connectionId</code> is set,
+ * the String <code>default</code> will be used</p>
  */
-@Typed()
-public class ConfigurableDataSource extends AbstractConfigurableDataSource
+public class ConfigurableDataSource implements DataSource
 {
-    private AbstractConfigurableDataSource configurableDataSource;
+    /**
+     * config and settings are loaded only once.
+     */
+    private boolean loaded;
 
     /**
-     * {@inheritDoc}
+     * The connectionId allows to configure multiple databases.
+     * This can e.g. be used to distinguish between a 'customer' and 'admin'
+     * database.
      */
-    protected DataSource resolveDataSource() throws SQLException
-    {
-        //only needed here because #configureDataSource will be called afterwards (in any case)
-        this.configurableDataSource = BeanManagerProvider.getInstance()
-                .getContextualReference(AbstractConfigurableDataSource.class);
+    private String connectionId = "default";
 
-        return this.configurableDataSource.resolveDataSource();
+    /**
+     * The underlying configuration of the datasource
+     */
+    private DataSourceConfig dataSourceConfig;
+
+    /**
+     *
+     */
+    private volatile DataSource wrappedDataSource;
+
+    public ConfigurableDataSource()
+    {
+        loaded = false;
+        dataSourceConfig = BeanManagerProvider.getInstance().getContextualReference(DataSourceConfig.class);
+    }
+
+    public void setConnectionId(String connectionId)
+    {
+        if (loaded)
+        {
+            throw new IllegalStateException("connectionId must not get changed after the DataSource was established");
+        }
+        this.connectionId = connectionId;
+    }
+
+    public Connection getConnection() throws SQLException
+    {
+        return getConnection(null, null);
+    }
+
+    public Connection getConnection(String userName, String password) throws SQLException
+    {
+        if (wrappedDataSource == null)
+        {
+            initDataSource();
+        }
+
+        if (userName == null && password == null)
+        {
+            return wrappedDataSource.getConnection();
+        }
+        return wrappedDataSource.getConnection(userName, password);
+    }
+
+
+    public PrintWriter getLogWriter() throws SQLException
+    {
+        return null;
+    }
+
+    public void setLogWriter(PrintWriter printWriter) throws SQLException
+    {
+    }
+
+    public void setLoginTimeout(int loginTimeout) throws SQLException
+    {
+    }
+
+    public int getLoginTimeout() throws SQLException
+    {
+        return 0;
+    }
+
+    public <T> T unwrap(Class<T> iface) throws SQLException
+    {
+        if (isWrapperFor(iface))
+        {
+            return (T) this;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public boolean isWrapperFor(Class<?> iface) throws SQLException
+    {
+        return iface.isAssignableFrom(ConfigurableDataSource.class);
     }
 
     /**
-     * {@inheritDoc}
+     * NEW JDK1.7 signature.
+     * This makes sure that CODI can also get compiled using java-7.
+     * This method is not actively used though.
      */
-    protected void configureDataSource(DataSource dataSource)
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException
     {
-        this.configurableDataSource.configureDataSource(dataSource);
+        throw new SQLFeatureNotSupportedException();
     }
+
+    /**
+     *
+     */
+    protected void initDataSource() throws SQLException
+    {
+        // double check lock idiom on volatile member is ok as of Java5
+        if (wrappedDataSource != null)
+        {
+            return;
+        }
+
+        String jndiLookupName = dataSourceConfig.getJndiResourceName(connectionId);
+        if (jndiLookupName != null && jndiLookupName.length() > 0)
+        {
+            wrappedDataSource = resolveDataSourceViaJndi(jndiLookupName);
+            return;
+        }
+
+
+        // no JNDI, so we take the direct JDBC route.
+        String jdbcDriverClass = dataSourceConfig.getDriverClassName(connectionId);
+        if (jdbcDriverClass == null && jdbcDriverClass.length() == 0)
+        {
+            throw new SQLException("Neither a JNDI location nor a JDBC driver class name is configured!");
+        }
+
+        try
+        {
+            Class clazz =  Class.forName(jdbcDriverClass);
+
+            // the given driver classname must be a DataSource
+            if (!DataSource.class.isAssignableFrom(clazz))
+            {
+                throw new SQLException("Configured DriverClassName is not a javax.sql.DataSource: "
+                                       + jdbcDriverClass);
+            }
+
+            wrappedDataSource = (DataSource) clazz.newInstance();
+
+            Map<String, String> config = dataSourceConfig.getConnectionProperties(connectionId);
+            for (Map.Entry<String, String> configOption : config.entrySet())
+            {
+                BeanUtils.setProperty(wrappedDataSource, configOption.getKey(), configOption.getValue());
+            }
+        }
+        catch (Exception e)
+        {
+            wrappedDataSource = null;
+
+            if (e instanceof SQLException)
+            {
+                throw (SQLException) e;
+            }
+            throw new SQLException(e);
+        }
+    }
+
+    protected DataSource resolveDataSourceViaJndi(String jndiLookupName)
+    {
+        DataSource ds = null;
+        try
+        {
+            Context jndiContext = new InitialContext();
+            ds = (DataSource) jndiContext.lookup(jndiLookupName);
+        }
+        catch (Exception e)
+        {
+            throw new IllegalArgumentException("Could not lookup DataSource from JNDI using " + jndiLookupName);
+        }
+
+        return ds;
+    }
+
 }
