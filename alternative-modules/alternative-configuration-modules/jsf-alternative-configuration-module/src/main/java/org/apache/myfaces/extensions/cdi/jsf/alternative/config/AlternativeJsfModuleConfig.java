209228b1717d032fe5cf55c9fd4e8d0f7cc26028
Gerhard Petracek <gpetracek@apache.org>
2011-11-04 13:33:20 +0000

    EXTCDI-238 cleanup of the alternative-jsf config module
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@1197553 13f79535-47bb-0310-9956-ffa450edef68

--- alternative-modules/alternative-configuration-modules/jsf-alternative-configuration-module/src/main/java/org/apache/myfaces/extensions/cdi/jsf/alternative/config/AlternativeJsfModuleConfig.java
+++ alternative-modules/alternative-configuration-modules/jsf-alternative-configuration-module/src/main/java/org/apache/myfaces/extensions/cdi/jsf/alternative/config/AlternativeJsfModuleConfig.java
@@ -28,7 +28,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * @author Gerhard Petracek
+ * Alternative implementation of {@link JsfModuleConfig}
  */
 @AlternativeImplementation
 public class AlternativeJsfModuleConfig extends JsfModuleConfig
