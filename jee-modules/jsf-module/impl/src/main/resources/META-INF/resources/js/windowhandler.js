b62cdf34a7d2529a1beb96183630ab4db927c9e5
Mark Struberg <struberg@apache.org>
2012-05-12 11:14:06 +0000

    EXTCDI-284 use less indenting to save 1kB
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/cdi/trunk@1337513 13f79535-47bb-0310-9956-ffa450edef68

--- jee-modules/jsf-module/impl/src/main/resources/META-INF/resources/js/windowhandler.js
+++ jee-modules/jsf-module/impl/src/main/resources/META-INF/resources/js/windowhandler.js
@@ -18,102 +18,102 @@
  */
 (function() {
 //wrapping so that all internal functions are privately scoped
-    function isHtml5() {
-        try {
-            return !!localStorage.getItem;
-        } catch(e) {
-            return false;
-        }
+function isHtml5() {
+    try {
+        return !!localStorage.getItem;
+    } catch(e) {
+        return false;
     }
+}
 
 // some browsers don't understand JSON - guess which one ... :(
-    function stringify(someArray) {
-        if (JSON) {
-            return JSON.stringify(someArray);
-        }
-        return someArray.join("|||");
+function stringify(someArray) {
+    if (JSON) {
+        return JSON.stringify(someArray);
     }
+    return someArray.join("|||");
+}
 
 // store the current body in the html5 localstorage
-    function storeWindowTree() {
-        // first we store all CSS we also need on the intermediate page
-        var headNodes = document.getElementsByTagName("head")[0].childNodes;
-        var oldSS = new Array();
-        var j = 0;
-        for (var i = 0; i < headNodes.length; i++) {
-            var tagName = headNodes[i].tagName;
-            if (tagName && equalsIgnoreCase(tagName, "link") &&
-                    equalsIgnoreCase(headNodes[i].getAttribute("type"), "text/css")) {
+function storeWindowTree() {
+    // first we store all CSS we also need on the intermediate page
+    var headNodes = document.getElementsByTagName("head")[0].childNodes;
+    var oldSS = new Array();
+    var j = 0;
+    for (var i = 0; i < headNodes.length; i++) {
+        var tagName = headNodes[i].tagName;
+        if (tagName && equalsIgnoreCase(tagName, "link") &&
+                equalsIgnoreCase(headNodes[i].getAttribute("type"), "text/css")) {
 
-                // sort out media="print" and stuff
-                var media = headNodes[i].getAttribute("media");
-                if (!media || equalsIgnoreCase(media, "all") || equalsIgnoreCase(media, 'screen')) {
-                    oldSS[j++] = headNodes[i].getAttribute("href");
-                }
+            // sort out media="print" and stuff
+            var media = headNodes[i].getAttribute("media");
+            if (!media || equalsIgnoreCase(media, "all") || equalsIgnoreCase(media, 'screen')) {
+                oldSS[j++] = headNodes[i].getAttribute("href");
             }
         }
-        localStorage.setItem(window.name + '_css', stringify(oldSS));
-        var body = document.getElementsByTagName("body")[0];
-        localStorage.setItem(window.name + '_body', body.innerHTML);
-        //X TODO: store ALL attributes of the body tag
-        localStorage.setItem(window.name + '_bodyAttrs', body.getAttribute("class"));
+    }
+    localStorage.setItem(window.name + '_css', stringify(oldSS));
+    var body = document.getElementsByTagName("body")[0];
+    localStorage.setItem(window.name + '_body', body.innerHTML);
+    //X TODO: store ALL attributes of the body tag
+    localStorage.setItem(window.name + '_bodyAttrs', body.getAttribute("class"));
+    return true;
+}
+
+function equalsIgnoreCase(source, destination) {
+    //either both are not set or null
+    if (!source && !destination) {
         return true;
     }
+    //source or dest is set while the other is not
+    if (!source || !destination) return false;
 
-    function equalsIgnoreCase(source, destination) {
-        //either both are not set or null
-        if (!source && !destination) {
-            return true;
-        }
-        //source or dest is set while the other is not
-        if (!source || !destination) return false;
+    //in any other case we do a strong string comparison
+    return source.toLowerCase() === destination.toLowerCase();
+}
 
-        //in any other case we do a strong string comparison
-        return source.toLowerCase() === destination.toLowerCase();
-    }
+function applyOnClick() {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        if (!links[i].onclick) {
+            links[i].onclick = function() {storeWindowTree(); return true;};
+        } else {
+            //the function wrapper is important
+            //otherwise the last onclick handler would be assigned
+            //to oldonclick
+            (function storeEvent() {
+                var oldonclick = links[i].onclick;
+                links[i].onclick = function(evt) {
+                    //ie handling added
+                    evt = evt || window.event;
 
-    function applyOnClick() {
-        var links = document.getElementsByTagName("a");
-        for (var i = 0; i < links.length; i++) {
-            if (!links[i].onclick) {
-                links[i].onclick = function() {storeWindowTree(); return true;};
-            } else {
-                //the function wrapper is important
-                //otherwise the last onclick handler would be assigned
-                //to oldonclick
-                (function storeEvent() {
-                    var oldonclick = links[i].onclick;
-                    links[i].onclick = function(evt) {
-                        //ie handling added
-                        evt = evt || window.event;
-
-                        return storeWindowTree() && oldonclick(evt);
-                    };
-                })();
-            }
+                    return storeWindowTree() && oldonclick(evt);
+                };
+            })();
         }
     }
+}
 
-    function assertWindowId() {
-        var freshWindow = window.name.length < 1;
-        if (freshWindow) {
-            var url = urlWithoutWindowId(window.location.href);
-            window.name = "window";
-            window.location = url;
-        }
+function assertWindowId() {
+    var freshWindow = window.name.length < 1;
+    if (freshWindow) {
+        var url = urlWithoutWindowId(window.location.href);
+        window.name = "window";
+        window.location = url;
     }
+}
 
-    var oldWindowOnLoad = window.onload;
+var oldWindowOnLoad = window.onload;
 
-    window.onload = function(evt) {
-        try {
-            (oldWindowOnLoad)? oldWindowOnLoad(evt): null;
-        } finally {
-            if (isHtml5()) {
-                applyOnClick();
-            }
-            // this ensures that even without the ClientSideWindowHandler
-            assertWindowId();
+window.onload = function(evt) {
+    try {
+        (oldWindowOnLoad)? oldWindowOnLoad(evt): null;
+    } finally {
+        if (isHtml5()) {
+            applyOnClick();
         }
+        // this ensures that even without the ClientSideWindowHandler
+        assertWindowId();
     }
+}
 })();
